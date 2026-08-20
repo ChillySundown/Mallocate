278d09663c8a56c22b0be7933109bc1b90ae3b6e
Abbas Darwich <fanamatic2.0@gmail.com>
2026-08-19 22:27:50 -0700

    Removed unnecessary starting_page id logic from popPages

--- PageHeap.cpp
+++ PageHeap.cpp
@@ -1,18 +1,27 @@
 #include "PageHeap.h"
 
-Span* pop_pages(Span* s, size_t page_length) {
+void PageHeap::init_arena(MetaArena* arena) {
+    mem_arena = arena;
+}
+
+Span* PageHeap::popPages(size_t index, size_t page_length) {
     //Assume that s->num_pages will never be smaller than page_length
+    Span* s = free_page_lists[index];
     if(s->num_pages == page_length) {
+        free_page_lists[index] = s->next;
         return s;
     } else {
-        Span new_span;
-        new_span.starting_page_id = s->starting_page_id + page_length;
-        new_span.num_pages = page_length;
+        Span* new_span = static_cast<Span*>(mem_arena->allocate(sizeof(Span)));
+        new_span->starting_page_id = s->starting_page_id + page_length;
+        new_span->num_pages = page_length;
 
+        free_page_lists[index] = s->next;
         s->num_pages -= page_length;
-        s->starting_page_id += page_length;
+        s->next = free_page_lists[s->num_pages - 1]; //Might cause index error
+        free_page_lists[s->num_pages - 1] = s;
+
         //s->starting_page_id -= page_length;
-        return &new_span;
+        return new_span;
     }
 }
 
@@ -21,16 +30,19 @@ Span* PageHeap::pageAlloc(size_t page_size) {
     if(size_index < 1) {return nullptr;}
     else if(size_index > 255) { size_index = 255;} //List of large pages
     
-    if(free_page_lists[size_index]) {
-        return pop_pages(free_page_lists[size_index], page_size);
+    if(free_page_lists[size_index]) { //First: try to pop from respective page list
+        Span* s = popPages(size_index, page_size);
+
     } else {
-        while(size_index < 255) {
+        while(size_index < 255) { //Second: Iterate through all larger page lists until a free page is found
             if(free_page_lists[size_index] != nullptr) {
-                return pop_pages(free_page_lists[size_index], page_size);
+                return popPages(size_index, page_size);
             } else {
                 size_index += 1;
             }
         }
-
     }
+
+    //Third: Allocate a chunk of memory that can be sliced and diced for pages
+
 }
--- PageHeap.h
+++ PageHeap.h
@@ -6,12 +6,14 @@
 class PageHeap {
     private:
         //Something something mutex
+        Span* popPages(size_t index, size_t page_length);
         Span* free_page_lists[256] {nullptr};
+        MetaArena* mem_arena {nullptr};
     public: 
+        void init_arena(MetaArena* arena);
         Span* pageAlloc(size_t num_pages);
         void pageFree(Span* pages, size_t length);
+    
 };
-
-Span* popPages(Span* s, size_t page_length);
 
 #endif
